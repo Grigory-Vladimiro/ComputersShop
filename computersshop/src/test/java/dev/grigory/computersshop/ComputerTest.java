c377fc70f7c291b187611f3984f0639fb81ec080
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-02-22 19:55:26 +0100

    test_add_computer

--- computersshop/src/test/java/dev/grigory/computersshop/ComputerTest.java
+++ computersshop/src/test/java/dev/grigory/computersshop/ComputerTest.java
@@ -1,4 +1,16 @@
 package dev.grigory.computersshop;
 
-public class ComputerTest {
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComputerStoreTest {
+    private final ComputerStore store = new ComputerStore("CyberDepot", "Grigory", "987654321");
+
+    @Test
+    void testAddAndFindComputer() {
+        Computer comp = new Computer("Lenovo", 16, "Intel i7", "Windows 10", 1200.0);
+        store.addComputer(comp);
+        assertNotNull(store.searchComputerByBrand("Lenovo"));
+    }
 }
